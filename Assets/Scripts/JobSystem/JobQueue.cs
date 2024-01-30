0f10ae9778715db93f3bb66c5c072c6941fbbc0f
Chris Kapinga <chris.kapinga@ordina.nl>
2024-01-30 21:13:40 +0100

    refactor: JobQueue, minor improvements in names and logic

--- Assets/Scripts/JobSystem/JobQueue.cs
+++ Assets/Scripts/JobSystem/JobQueue.cs
@@ -4,46 +4,32 @@ namespace MyRTSGame.Model
 {
     public class JobQueue
     {
-        // Singleton instance
-        private static JobQueue instance;
-
-        // List of jobs
-        private List<Job> jobs;
+        private static JobQueue _instance;
+        private readonly List<Job> _jobs;
 
         // Private constructor to prevent instantiation
         private JobQueue()
         {
-            jobs = new List<Job>();
+            _jobs = new List<Job>();
         }
 
-        // Public method to get the singleton instance
         public static JobQueue GetInstance()
         {
-            if (instance == null)
-            {
-                instance = new JobQueue();
-            }
-
-            return instance;
+            return _instance ??= new JobQueue();
         }
 
-        // Method to add a job to the queue
         public void AddJob(Job job)
         {
-            jobs.Add(job);
+            _jobs.Add(job);
         }
 
-        // Method to get the next job from the queue
         public Job GetNextJob()
         {
-            if (jobs.Count > 0)
-            {
-                Job job = jobs[0];
-                jobs.RemoveAt(0);
-                return job;
-            }
-
-            return null;
+            if (_jobs.Count <= 0) return null;
+            
+            var job = _jobs[0];
+            _jobs.RemoveAt(0);
+            return job;
         }
     }
 
