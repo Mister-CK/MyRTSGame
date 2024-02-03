0edc87d286b8aa56c1d4dc45afd6b9b61fa05e0c
Chris Kapinga <chris.kapinga@ordina.nl>
2024-02-04 00:32:44 +0100

    refactor: small clean up

--- Assets/Scripts/Buildings/BuildingList.cs
+++ Assets/Scripts/Buildings/BuildingList.cs
@@ -5,7 +5,6 @@ namespace MyRTSGame.Model
 {
     public class BuildingList : MonoBehaviour
     {
-        // [SerializeField] private TextMeshProUGUI textComponent;
         private List<Building> _buildings;
         public static BuildingList Instance { get; private set; }
         private bool _firstWarehouse = true;
@@ -33,30 +32,6 @@ namespace MyRTSGame.Model
 
             _buildings = new List<Building>(FindObjectsOfType<Building>());
         }
-
-        // void Update()
-        // {
-        //     // UpdateList();
-        // }
-        // private void UpdateList()
-        // {
-        //     string buildingList = "";
-        //     foreach (Building building in _buildings)
-        //     {
-        //         Resource[] inventory = building.GetInventory();
-        //         string inventoryText = GetTextForInventory(inventory);
-        //         buildingList += building.BuildingType + ": " + inventoryText + "\n";
-        //     }
-        //     textComponent.text = buildingList;
-        // }
-
-        // private static string GetTextForInventory(Resource[] inventory) {
-        //     string inventoryText = "";
-        //     foreach (Resource resource in inventory) {
-        //         inventoryText += resource.ResourceType + ":" + resource.Quantity + " ";
-        //     }
-        //     return inventoryText;
-        // }
 
         public List<Building> GetBuildings()
         {
--- Assets/Scripts/Buildings/BuildingPlacer.cs
+++ Assets/Scripts/Buildings/BuildingPlacer.cs
@@ -11,44 +11,44 @@ namespace MyRTSGame.Model
 
         private void Update()
         {
-            if (_isPlacing)
+            if (!_isPlacing) return;
+            
+            var placingState = (PlacingState)_building.GetState();
+            placingState.CheckOverlap(_building);
+
+            // Create a ray from the camera going through the mouse position
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // Perform the raycast
+            if (Physics.Raycast(ray, out var hit))
             {
-                var placingState = (PlacingState)_building.GetState();
-                placingState.CheckOverlap(_building);
+                // Define the size of the grid
+                var gridSize = 1.0f;
 
-                // Create a ray from the camera going through the mouse position
-                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                // Round the x and z coordinates to the nearest grid size
+                var x = Mathf.Round(hit.point.x / gridSize) * gridSize;
+                var z = Mathf.Round(hit.point.z / gridSize) * gridSize;
 
-                // Perform the raycast
-                if (Physics.Raycast(ray, out var hit))
-                {
-                    // Define the size of the grid
-                    var gridSize = 1.0f;
+                // If the ray hits something, move the building to the grid intersection
+                _building.transform.position = new Vector3(x, 0, z);
+            }
 
-                    // Round the x and z coordinates to the nearest grid size
-                    var x = Mathf.Round(hit.point.x / gridSize) * gridSize;
-                    var z = Mathf.Round(hit.point.z / gridSize) * gridSize;
-
-                    // If the ray hits something, move the building to the grid intersection
-                    _building.transform.position = new Vector3(x, 0, z);
-                }
-
-                // If the mouse button is clicked, place the building
-                if (Input.GetMouseButtonDown(0))
-                    if (_building.Material.color == Color.green)
-                    {
-                        _isPlacing = false;
-                        _building.SetState(new FoundationState(_building.BuildingType));
-                        navMeshSurface.BuildNavMesh();
-                    }
-
-                // If the right mouse button is clicked, cancel the placement
-                if (Input.GetMouseButtonDown(1))
+            // If the mouse button is clicked, place the building
+            if (Input.GetMouseButtonDown(0))
+                if (_building.Material.color == Color.green)
                 {
                     _isPlacing = false;
-                    Destroy(_building.gameObject);
+                    _building.SetState(new FoundationState(_building.BuildingType));
+                    navMeshSurface.BuildNavMesh();
                 }
+
+            // If the right mouse button is clicked, cancel the placement
+            if (Input.GetMouseButtonDown(1))
+            {
+                _isPlacing = false;
+                Destroy(_building.gameObject);
             }
+            
         }
 
 
