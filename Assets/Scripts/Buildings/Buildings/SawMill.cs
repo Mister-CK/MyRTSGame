c99bf97db601f6907956b6c891968abbb25fa088
Chris Kapinga <chris.kapinga@ordina.nl>
2024-02-04 12:07:03 +0100

    refactor: create buildingsFolder, remove unused imports

---
+++ Assets/Scripts/Buildings/Buildings/SawMill.cs
@@ -0,0 +1,35 @@
+namespace MyRTSGame.Model
+{
+    public class SawMill : Building
+    {
+        //Constructor
+        public SawMill()
+        {
+            BuildingType = BuildingType.SawMill;
+        }
+
+        protected override void Start()
+        {            
+            capacityForCompletedBuilding = 5;
+            resourceCountNeededForConstruction = 3;
+            JobController = JobController.GetInstance();
+            BuildingList = BuildingList.Instance; 
+            SelectionManager = SelectionManager.Instance;
+            State = new PlacingState(BuildingType);
+            ResourceType[] resourceTypes = { ResourceType.Lumber, ResourceType.Wood };
+            int[] resourceQuantities = { 0, 0 };
+            Inventory = InitInventory(resourceTypes, resourceQuantities);
+            InventoryWhenCompleted = InitInventory(resourceTypes, resourceQuantities);
+            InputTypesWhenCompleted = new[] { ResourceType.Lumber };
+            Capacity = 5;
+            HasInput = true;
+        }
+        
+        public override void StartResourceCreationCoroutine()
+        {
+            Resource[] input = { new() { ResourceType = ResourceType.Lumber, Quantity = 1 } };
+            Resource[] output = { new() { ResourceType = ResourceType.Wood, Quantity = 1 } };
+            StartCoroutine(BuildingController.CreateOutputFromInput(10, input, output));
+        }
+    }
+}
---
+++ Assets/Scripts/Buildings/Buildings/StoneQuarry.cs
@@ -0,0 +1,31 @@
+namespace MyRTSGame.Model
+{
+    public class StoneQuarry : Building
+    {
+        //Constructor
+        public StoneQuarry()
+        {
+            BuildingType = BuildingType.StoneQuarry;
+        }
+
+        protected override void Start()
+        {
+            capacityForCompletedBuilding = 5;
+            resourceCountNeededForConstruction = 3;
+            JobController = JobController.GetInstance();
+            BuildingList = BuildingList.Instance; 
+            SelectionManager = SelectionManager.Instance;
+            State = new PlacingState(BuildingType);
+            ResourceType[] resourceTypes = { ResourceType.Stone };
+            int[] resourceQuantities = { 0 };
+            Inventory = InitInventory(resourceTypes, resourceQuantities);
+            InventoryWhenCompleted = InitInventory(resourceTypes, resourceQuantities);
+            Capacity = 5;
+        }
+
+        public override void StartResourceCreationCoroutine()
+        {
+            StartCoroutine(BuildingController.CreateResource(15, ResourceType.Stone));
+        }
+    }
+}
