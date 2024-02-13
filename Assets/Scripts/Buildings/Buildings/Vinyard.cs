a999519e54364b66291d839158a88de2e449e048
Chris Kapinga <chris.kapinga@ordina.nl>
2024-02-13 01:05:37 +0100

    feat: add script for Vineyard

---
+++ Assets/Scripts/Buildings/Buildings/Vinyard.cs
@@ -0,0 +1,25 @@
+namespace MyRTSGame.Model
+{
+    public class Vineyard : Building
+    {
+        //Constructor
+        public Vineyard()
+        {
+            BuildingType = BuildingType.Vineyard;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            ResourceType[] resourceTypes = { ResourceType.Wine };
+            var resourceQuantities = new int[resourceTypes.Length];
+            InventoryWhenCompleted = InitInventory(resourceTypes, resourceQuantities);
+        }
+
+        public override void StartResourceCreationCoroutine()
+        {
+            StartCoroutine(buildingController.CreateResource(this, 15, ResourceType.Wine));
+        }
+    }
+}
