06551bcb94c3c7950050b4e602c158a18c225b51
Chris Kapinga <chris.kapinga@ordina.nl>
2024-02-16 23:39:08 +0100

    refactor: improve structure of SelectionView

--- Assets/Scripts/UI/SelectionView.cs
+++ Assets/Scripts/UI/SelectionView.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using MyRTSGame.Model;
 using TMPro;
@@ -12,33 +13,42 @@ public class SelectionView : MonoBehaviour
 
     public void UpdateView(ISelectable selectable)
     {
-        if (selectable is Building building)
+        switch (selectable)
         {
-            var text = building.BuildingType + "\n" +
-                       GetTextForInventory(building.GetInventory()) + "\n" +
-                       GetTextForInputTypes(building.InputTypes) + "\n" +
-                       "Capacity: " + building.GetCapacity() + "\n" +
-                       GetTextForResourcesInJobsForBuilding(building.GetResourcesInJobForBuilding());
+            case Building building:
+                SetSelectedBuilding(building);
+                break;
+            case Villager villager:
+                SetSelectedVillager(villager);
+                break;
+        }
+    }
+
+    private void SetSelectedBuilding(Building building)
+    {
+        var text = building.BuildingType + "\n" +
+                   GetTextForInventory(building.GetInventory()) + "\n" +
+                   GetTextForInputTypes(building.InputTypes) + "\n" +
+                   "Capacity: " + building.GetCapacity() + "\n" +
+                   GetTextForResourcesInJobsForBuilding(building.GetResourcesInJobForBuilding());
                            
-            textComponent.text = text;
-            SetDeleteButton(true);
-            return;
-        }
-        
-        if (selectable is Villager villager)
-        {
-            var text = "Villager" + "\n";
-            var job = villager.GetCurrentJob();
-            var destinationString = villager.GetHasDestination() ? "hasDestination" : "noDestination" + "\n";
-            if (job != null)
-                text += job.Origin.BuildingType + "\n" +
-                        job.ResourceType + "\n" +
-                        job.Destination.BuildingType + "\n" +
-                        destinationString;
-            textComponent.text = text;
-            SetDeleteButton(true);
-            return;
-        }
+        textComponent.text = text;
+        SetDeleteButton(true);
+    }
+    
+    //TODO: refactor this method to work for all units.
+    private void SetSelectedVillager(Villager villager)
+    {
+        var text = "Villager" + "\n";
+        var job = villager.GetCurrentJob();
+        var destinationString = villager.GetHasDestination() ? "hasDestination" : "noDestination" + "\n";
+        if (job != null)
+            text += job.Origin.BuildingType + "\n" +
+                    job.ResourceType + "\n" +
+                    job.Destination.BuildingType + "\n" +
+                    destinationString;
+        textComponent.text = text;
+        SetDeleteButton(true);
     }
 
     private void SetDeleteButton(bool show)
