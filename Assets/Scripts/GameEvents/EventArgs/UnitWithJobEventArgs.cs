4a5dd23f44f62b01ac43d9eafb8ca956dfbac31e
Chris Kapinga <chris.kapinga@ordina.nl>
2024-04-20 15:00:03 +0200

    chore: remove unused event args

--- Assets/Scripts/GameEvents/EventArgs/UnitWithJobEventArgs.cs
+++ Assets/Scripts/GameEvents/EventArgs/UnitWithJobEventArgs.cs
@@ -5,8 +5,7 @@ public class UnitWithJobEventArgs : EventArgs, IGameEventArgs
 {
     public Unit Unit { get; }
     public Job Job { get; }
-
-
+    
     public UnitWithJobEventArgs(Unit unit, Job job)
     {
         Unit = unit;
